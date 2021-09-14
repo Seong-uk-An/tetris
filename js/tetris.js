279f0db08b56dda7121cd0110a852129fed62fb7
Seong-uk-An <asu970602@naver.com>
2021-09-14 18:26:13 +0900

    tetris

--- js/tetris.js
+++ js/tetris.js
@@ -1,4 +1,9 @@
+import BLOCKS from "./blocks.js";
+
 const playground = document.querySelector(".playground > ul");
+const gameText = document.querySelector(".game-text");
+const scoreDisplay = document.querySelector(".score");
+const retryButton = document.querySelector(".game-text > button");
 
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
@@ -8,25 +13,11 @@ let duration = 500;
 let downInterval;
 let tempMovingItem;
 
-const BLOCKS = {
-  tree: [
-    [
-      [0, 0],
-      [0, 1],
-      [1, 0],
-      [1, 1],
-    ],
-    [],
-    [],
-    [],
-  ],
-};
-
 const movingItem = {
-  type: "tree",
+  type: "",
   direction: 0,
   top: 0,
-  left: 4,
+  left: 3,
 };
 
 init();
@@ -34,41 +25,136 @@ init();
 function init() {
   tempMovingItem = { ...movingItem };
   for (let i = 0; i < GAME_ROWS; i++) {
-    appendNewline();
+    prependNewline();
   }
-  renderBlocks();
+  generateNewBlock();
 }
 
-function appendNewline() {
+function prependNewline() {
   const li = document.createElement("li");
   const ul = document.createElement("ul");
   for (let j = 0; j < GAME_COLS; j++) {
     const matrix = document.createElement("li");
-    ul.appendChild(matrix);
+    ul.prepend(matrix);
   }
-  li.appendChild(ul);
-  playground.appendChild(li);
+  li.prepend(ul);
+  playground.prepend(li);
 }
 
-function renderBlocks() {
+function renderBlocks(moveType = "") {
   const { type, direction, top, left } = tempMovingItem;
   const movingBlocks = document.querySelectorAll(".moving");
   movingBlocks.forEach((moving) => {
-    console.log(moving);
     moving.classList.remove(type, "moving");
   });
 
-  BLOCKS[type][direction].forEach((block) => {
+  BLOCKS[type][direction].some((block) => {
     const x = block[0] + left;
     const y = block[1] + top;
-    const target = playground.childNodes[y].childNodes[0].childNodes[x];
-    target.classList.add(type, "moving");
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
+      tempMovingItem = { ...movingItem };
+      if (moveType === "retry") {
+        clearInterval(downInterval);
+        showGameoverText();
+      }
+      setTimeout(() => {
+        renderBlocks("retry");
+        if (moveType === "top") {
+          seizeBlock();
+        }
+      }, 0);
+      return true;
+    }
   });
+
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
+}
+
+function seizeBlock() {
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove("moving");
+    moving.classList.add("seized");
+  });
+  checkMatch();
+}
+
+function checkMatch() {
+  const childNodes = playground.childNodes;
+  childNodes.forEach((child) => {
+    let matched = true;
+    console.log({ child });
+    child.childNodes[0].childNodes.forEach((li) => {
+      if (!li.classList.contains("seized")) {
+        matched = false;
+      }
+    });
+    console.log(matched);
+    if (matched) {
+      child.remove();
+      prependNewline();
+      score += 10;
+      scoreDisplay.innerHTML = score;
+    }
+  });
+
+  generateNewBlock();
+}
+
+function generateNewBlock() {
+  clearInterval(downInterval);
+  downInterval = setInterval(() => {
+    moveBlock("top", 1);
+  }, duration);
+
+  const blockArray = Object.entries(BLOCKS);
+  const randomIndex = Math.floor(Math.random() * blockArray.length);
+
+  movingItem.type = blockArray[randomIndex][0];
+  movingItem.top = 0;
+  movingItem.left = 3;
+  movingItem.direction = 0;
+  tempMovingItem = { ...movingItem };
+  renderBlocks();
+}
+
+function checkEmpty(target) {
+  if (!target || target.classList.contains("seized")) {
+    return false;
+  }
+  return true;
 }
 
 function moveBlock(moveType, amount) {
   tempMovingItem[moveType] += amount;
+  renderBlocks(moveType);
+}
+
+function changeDirection() {
+  const direction = tempMovingItem.direction;
+  direction === 3
+    ? (tempMovingItem.direction = 0)
+    : (tempMovingItem.direction += 1);
   renderBlocks();
+}
+
+function dropBlock() {
+  clearInterval(downInterval);
+  downInterval = setInterval(() => {
+    moveBlock("top", 1);
+  }, 10);
+}
+
+function showGameoverText() {
+  gameText.style.display = "flex";
 }
 
 document.addEventListener("keydown", (e) => {
@@ -81,15 +167,27 @@ document.addEventListener("keydown", (e) => {
       moveBlock("left", -1);
       break;
 
-    case 37:
-      moveBlock("left", -1);
+    case 40:
+      moveBlock("top", 1);
       break;
 
-    case 37:
-      moveBlock("left", -1);
+    case 38:
+      changeDirection();
+      break;
+
+    case 32:
+      dropBlock();
       break;
 
     default:
       break;
   }
+});
+
+retryButton.addEventListener("click", () => {
+  score = 0;
+  scoreDisplay.innerHTML = 0;
+  playground.innerHTML = "";
+  gameText.style.display = "none";
+  init();
 });
